adbc0e0cc5aca46b9589f7fcb859f4bd6d68a911
matejbivr <matej.pretnar@protonmail.ch>
2016-11-24 18:05:49 +0100

    feat(menu): working slide menu sans styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { routing } from './app.routes';
@@ -28,7 +29,8 @@ import { ContactComponent } from './contact/contact.component';
     BrowserModule,
     FormsModule,
     routing,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
